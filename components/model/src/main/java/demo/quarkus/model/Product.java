6ef6aaf3812a3feda88896388bea9586d8f26f88
mostafacs <engmostafa.cs@gmail.com>
2020-09-11 20:14:28 +0200

    bigdecimal native image build

---
+++ components/model/src/main/java/demo/quarkus/model/Product.java
@@ -0,0 +1,35 @@
+package demo.quarkus.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sku;
+
+    private String title;
+
+    private String description;
+
+    private BigDecimal salePrice;
+
+    private BigDecimal costPrice;
+
+}
